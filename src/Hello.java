802702aaea5bdc084e048c843afb40ffef365769
SorryPip <yu2super@gmail.com>
2022-08-22 22:21:22 +0100

    Update Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -8,6 +8,12 @@ public class Hello {
 		
 		Dog doge = new Dog("german sheperd", "rover");
 		doge.bark();
+		
+		int count = 0;
+		for(int i = 0; i <= 10 ; ++i) {
+			System.out.println(i+" "+count);
+			count += 1;
+		}
 	}
 
 }
